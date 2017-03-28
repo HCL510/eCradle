c3f78d24a9f7e84dc3f53254501d23fe956b8e0f
HCL510 <haklim510@gmail.com>
2017-03-28 15:24:12 -0400

    Added SDK

---
+++ app/controllers/videos_controller.rb
@@ -0,0 +1,21 @@
+class VideosController < ApplicationController
+  def new
+  end
+
+  def index
+    
+    @videos = current_user.videos.where(approved: true)
+  end
+
+  def destroy
+    video = current_user.videos.find_by(uid: params[:id])
+    if video
+      ziggeo = Ziggeo.new(ENV['ZIGGEO_KEY'], ENV['ZIGGEO_SECRET'], ENV['ZIGGEO_ENCRYPTION'])
+      ziggeo.videos.delete(video.uid)
+      flash[:success] = 'Video removed! It may take some time to reflect changes on the website.'
+    else
+      flash[:warning] = 'Cannot find such video...'
+    end
+    redirect_to root_path
+  end
+end
---
+++ db/migrate/20170328172340_create_videos.rb
@@ -0,0 +1,13 @@
+class CreateVideos < ActiveRecord::Migration[5.0]
+  def change
+    create_table :videos do |t|
+      t.belongs_to :user, foreign_key: true
+      t.string :uid
+      t.decimal :duration
+      t.datetime :ziggeo_created_at
+      t.boolean :approved
+
+      t.timestamps
+    end
+  end
+end
